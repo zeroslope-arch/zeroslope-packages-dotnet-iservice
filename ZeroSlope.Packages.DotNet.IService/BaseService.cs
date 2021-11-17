4fc5077b448a246d1db8a6850f01459fd7583274
cfryerdev <cfryerdev@gmail.com>
2021-11-17 14:17:36 -0700

    Initial commit

---
+++ ZeroSlope.Packages.DotNet.IService/BaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroSlope.Packages.DotNet.IService
+{
+    public partial class BaseService : IService
+    {
+    }
+}
---
+++ ZeroSlope.Packages.DotNet.IService/Installers/IServiceInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Scrutor;
+
+namespace ZeroSlope.Packages.DotNet.IService.Installers
+{
+    public class IServiceInstaller
+    {
+        private readonly Type _typeInit;
+
+        public IServiceInstaller(Type initType)
+        {
+            _typeInit = initType;
+        }
+
+        public void Install(ITypeSourceSelector scan)
+        {
+            var ServiceAssembly = _typeInit.GetTypeInfo().Assembly;
+			scan.FromAssemblies(ServiceAssembly)
+				.AddClasses(classes => classes.AssignableTo<IService>())
+				.AsSelf()
+				.WithScopedLifetime();
+        }
+    }
+}
